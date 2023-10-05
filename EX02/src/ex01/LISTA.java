b1a1b25f20f2c50164cee4bae3662d6a7949f359
Pedro Augusto Vieira Santos <augusto.p@academico.ifg.edu.br>
2023-10-04 22:11:42 -0300

    atualizando EX02 com ex01 final

--- EX02/src/ex01/LISTA.java
+++ EX02/src/ex01/LISTA.java
@@ -4,6 +4,8 @@
  */
 package ex01;
 
+import java.awt.BorderLayout;
+
 /**
  *
  * @author 20221050100070
@@ -50,16 +52,59 @@ public class LISTA {
             } 
         }
 }
-    //Antecessor e sucessor de um nó escolhido pelo usuário. Caso não exista antecessor e/ou sucessor, informar na tela a inexistência.NOME DO MÉTODO DEVERÁ SER antecessorSucessor
+/*Antecessor e sucessor de um nó escolhido pelo usuário. Caso não exista antecessor e/ou sucessor, informar na tela 
+a inexistência.NOME DO MÉTODO DEVERÁ SER antecessorSucessor.*/
+    public void antecessorSucessor(int value){
+        NO aux = primeiro;
+        if(aux==null){
+            System.out.println("Lista Vazia");
+            return;
+        }
+        
+        NO antecessor = null;
+        NO sucessor = null;
+        
+        while(aux!=null){
+            if(aux.valor==value){
+                if(antecessor!=null){
+                    System.out.println("\nAntecessor:"+antecessor.valor);
+                }else{
+                    System.out.println("\nNão Tem Antecessor");
+                }
+                if(aux.prox!=null){
+                    System.out.println("\nSucessor:"+aux.prox.valor);
+                }else{
+                    System.out.println("\nNão Tem Sucessor");
+                }
+                return;
+            }
+            antecessor = aux;
+            aux = aux.prox;
+            if(aux!=null){
+                sucessor=aux;
+            }
+            
+            
+        }
+        System.out.println("NÓ não encontrado na lista");
+}
+  
     public static void main(String[] args) {
         LISTA lista = new LISTA();
-        
-        NO no1 = new NO(10);
-        NO no2 = new NO(8);
+     
+        NO no1 = new NO(1);
+        NO no2 = new NO(2);
+        NO no3 = new NO(3);
+        NO no4 = new NO(4);
+        NO no5 = new NO(5);
         
         lista.InserirNoFinal(no1);
         lista.InserirNoFinal(no2);
+        lista.InserirNoFinal(no3);
+        lista.InserirNoFinal(no4);
+        lista.InserirNoFinal(no5);
         lista.calcularMedia();
+        lista.antecessorSucessor(5);
     }
     
 }
